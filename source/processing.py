12223b759fd3e1a89b284194764c3498abd39e89
Kokc80 <kokc@inbox.ru>
2025-09-17 08:34:34 +0900

    убрал проверочные принты

--- source/processing.py
+++ source/processing.py
@@ -19,7 +19,7 @@ List_unsorted=[{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:2
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-print(sort_by_date(List_unsorted, data_key = 'date', descending = True))
+#print(sort_by_date(List_unsorted, data_key = 'date', descending = True))
 
 #print(filter_by_state(List_unsorted))
 
