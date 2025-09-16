9fe372d974505bdd894da06933f309bedde860cd
Kokc80 <kokc@inbox.ru>
2025-09-16 18:23:04 +0900

    removed .idea add gitignore change structure

---
+++ source/processing.py
@@ -0,0 +1,21 @@
+from typing import List, Dict
+print ("processing")
+def filter_by_state(banking_operations: List[Dict[str, str]], state: str = 'EXECUTED') -> List:
+    """ Функция принимает на вход список словарей с данными о банковских операциях
+    и параметр state, возвращает новый список, содержащий только те словари,
+      у которых ключ state содержит переданное в функцию значение """
+    filtered_list = []
+    for dict_item in banking_operations:
+        if dict_item.get('state') == state:
+            filtered_list.append(dict_item)
+    return filtered_list
+
+def sort_by_date(data_list: list, data_key, descending=True) -> list:
+  list = sorted(iterable=data_list, key=data_key, reverse=descending).
+  return(list)
+
+#проверка
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
