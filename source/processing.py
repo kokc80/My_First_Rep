c270a912335cd1a1b411439abdbf5d562832f14b
Kokc80 <kokc@inbox.ru>
2025-09-17 11:39:58 +0900

    Исправление ошибок

--- source/processing.py
+++ source/processing.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import Dict, List
 
 
 def filter_by_state(
@@ -7,28 +7,29 @@ def filter_by_state(
     """Функция принимает на вход список словарей с данными о банковских
     операциях и параметр state, возвращает новый список, содержащий только
     те словари, у которых ключ state содержит переданное в функцию значение"""
-    filtered_list = []
+    filtered_list: List = []
     for dict_item in banking_operations:
         if dict_item.get("state") == state:
             filtered_list.append(dict_item)
     return filtered_list
 
 
-def sort_by_date(data_list: list, data_key, descending=True) -> List:
+def sort_by_date(data_list: list, reverse: bool = True) -> List:
     """Функия принимает список словарей и необязательный параметр,
-        задающий порядок сортировки (по умолчанию — убывание). Функция
-        должна возвращать новый список, отсортированный по дате (date)
+    задающий порядок сортировки (по умолчанию — убывание). Функция
+    должна возвращать новый список, отсортированный по дате (date)
     )"""
-    List_sorted = sorted(data_list, key=lambda x: x[data_key], reverse=descending)
-    return List_sorted
+    list_sorted: List = sorted(data_list, key=lambda x: x.get("date"), reverse=True)
+    return list_sorted
 
 
-List_unsorted = [
+list_unsorted: List = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
-# print(sort_by_date(List_unsorted, data_key = 'date', descending = True))
-# print(filter_by_state(List_unsorted))
+# тестовый вывод
+# print(sort_by_date(list_unsorted, reverse=True))
+# print(filter_by_state(list_unsorted))
