1a8a3546d304a6a456bfde0727d3fb6e95f33a41
Kokc80 <kokc@inbox.ru>
2025-09-20 08:59:56 +0900

    написаны тесты для masks.get_mask_card_number

--- source/processing.py
+++ source/processing.py
@@ -1,9 +1,7 @@
 from typing import Dict, List
 
 
-def filter_by_state(
-    banking_operations: List[Dict[str, str]], state: str = "EXECUTED"
-) -> List:
+def filter_by_state(banking_operations: List[Dict[str, str]], state: str = "EXECUTED") -> List:
     """Функция принимает на вход список словарей с данными о банковских
     операциях и параметр state, возвращает новый список, содержащий только
     те словари, у которых ключ state содержит переданное в функцию значение"""
@@ -23,12 +21,12 @@ def sort_by_date(data_list: list, reverse: bool = True) -> List:
     return list_sorted
 
 
-list_unsorted: List = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
+#list_unsorted: List = [
+#    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+#    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+#    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+#    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+#]
 
 # тестовый вывод
 # print(sort_by_date(list_unsorted, reverse=True))
